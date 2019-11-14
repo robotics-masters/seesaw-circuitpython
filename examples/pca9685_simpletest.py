72ed3b5f1f6e91ae4b1f9a645e53008d1c0cfb11
wallarug.rpi <wallarug@coliemore.com.au>
2019-11-14 12:23:25 +0000

    updates to examples

--- examples/pca9685_simpletest.py
+++ examples/pca9685_simpletest.py
@@ -11,11 +11,14 @@ from adafruit_pca9685 import PCA9685
 i2c_bus = busio.I2C(SCL, SDA)
 
 # Create a simple PCA9685 class instance.
+print("setup")
 pca = PCA9685(i2c_bus)
 
 # Set the PWM frequency to 60hz.
+print("set frequency")
 pca.frequency = 60
 
 # Set the PWM duty cycle for channel zero to 50%. duty_cycle is 16 bits to match other PWM objects
 # but the PCA9685 will only actually give 12 bits of resolution.
-pca.channels[0].duty_cycle = 0x7fff
+print("channel 0 duty cycle")
+pca.channels[2].duty_cycle = 55555 
